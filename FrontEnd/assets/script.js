00481bbc85065d062eee619db03c6050bdf52dff
liujiaodaliao <mengmeijia@126.com>
2023-10-16 21:19:57 +0200

    add and change the sorting of projects by category in the gallery

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,4 +1,103 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+// filter 
+const gallery = document.querySelector('.gallery');
+  const filterButtons = document.querySelectorAll('.filter-button');
+  let currentCategory = '';
+  let projectsData = []; // 用于存储项目数据
+
+  // 步骤 4: 筛选项目
+  function filterProjects(category) {
+    if (category === 'tous') {
+      // 如果点击的是 "Tous" 按钮，直接显示所有项目
+      displayAllProjects(projectsData);
+    } else {
+      // 否则，根据选定的类别筛选项目
+      const filteredProjects = projectsData.filter((project) => project.name === category);
+      displayFilteredProjects(filteredProjects);
+    }
+  }
+
+  // 创建并显示筛选后的项目
+  function displayFilteredProjects(filteredProjects) {
+    gallery.innerHTML = '';
+    filteredProjects.forEach((project) => {
+      const figure = document.createElement('figure');
+      const image = document.createElement('img');
+      image.src = project.image;
+      image.alt = project.figcaption;
+      const figcaption = document.createElement('figcaption');
+      figcaption.textContent = project.figcaption;
+
+      figure.appendChild(image);
+      figure.appendChild(figcaption);
+      gallery.appendChild(figure);
+    });
+  }
+
+  // 直接显示所有项目
+  function displayAllProjects(data) {
+    gallery.innerHTML = '';
+    data.forEach((project) => {
+      const figure = document.createElement('figure');
+      const image = document.createElement('img');
+      image.src = project.image;
+      image.alt = project.figcaption;
+      const figcaption = document.createElement('figcaption');
+      figcaption.textContent = project.figcaption;
+
+      figure.appendChild(image);
+      figure.appendChild(figcaption);
+      gallery.appendChild(figure);
+    });
+  }
+
+  // 步骤 3: 创建筛选按钮（添加事件监听器）
+  filterButtons.forEach((button) => {
+    button.addEventListener('click', function () {
+      const selectedCategory = this.id;
+      console.log('选定的类别：', selectedCategory);
+
+      // 检查是否已经选中相同类别，如果是则返回
+      if (selectedCategory === currentCategory) {
+        return;
+      }
+
+      // 移除上一个按钮的激活状态
+      const currentActiveButton = document.querySelector('.filter-button.active');
+      if (currentActiveButton) {
+        currentActiveButton.classList.remove('active');
+      }
+
+      // 更新当前选中的类别
+      currentCategory = selectedCategory;
+
+      // 给当前按钮添加激活状态
+      this.classList.add('active');
+
+      // 调用筛选项目函数，并传递所选的类别
+      filterProjects(currentCategory);
+    });
+  });
+
+  // 初始化页面加载数据
+  loadData();
+
+  function loadData() {
+    fetch('galleries-auto.json')
+      .then((response) => response.json())
+      .then((data) => {
+        projectsData = data; // 将数据存储到全局变量
+        console.log('加载的数据：', projectsData);
+        // 页面加载后显示所有项目
+        displayAllProjects(projectsData);
+      })
+      .catch((error) => {
+        console.error('加载数据失败：', error);
+      });
+  }
+
+
 // Get login status
 // const email = localStorage.getItem('email');
 const userId = localStorage.getItem('userId');
@@ -23,5 +122,11 @@ loginLink.addEventListener('click', function () {
     }
   });
 
+
+
+
+
+
+  
 
 });
